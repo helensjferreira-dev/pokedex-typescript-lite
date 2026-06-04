4f114d680e5864c1f40b8f8d323e6d8ca4905bae
Hélen <helensjferreira@gmail.com>
2026-06-04 17:27:48 -0300

    feat: create formatter utilities

---
+++ src/utils/formatters.ts
@@ -0,0 +1,7 @@
+import { PokemonResumo } from "../models/Pokemon";
+
+export function formatarPokemon(pokemon: PokemonResumo): string {
+    return `
+    #${pokemon.id} - ${pokemon.nome} | Tipos: ${pokemon.tipos.join(", ")} | Altura: ${pokemon.altura} | Peso: ${pokemon.peso}`;
+
+}
