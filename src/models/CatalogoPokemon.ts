96baf80f49c5da5894cb020e2290a84fcd37a4b6
Hélen <helensjferreira@gmail.com>
2026-06-04 11:33:10 -0300

    feat: implement catalog operations

--- src/models/CatalogoPokemon.ts
+++ src/models/CatalogoPokemon.ts
@@ -2,4 +2,38 @@ import { PokemonResumo } from "./Pokemon";
 
 export class CatalogoPokemon {
     private pokemons: PokemonResumo[] = [];
+
+    // Methods
+    adicionar(pokemon: PokemonResumo): void  {
+        const jaExiste = this.pokemons.some((item) => item.id === pokemon.id);
+
+        if (jaExiste) {
+            console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
+            return;
+        }
+        this.pokemons.push(pokemon);
+        console.log(`[OK] ${pokemon.nome} (ID: ${pokemon.id}) adicionado ao catálogo.`);
+    }
+    listar(): void {
+        if (this.pokemons.length === 0) {
+            console.log(`[AVISO] Catálogo vazio.`);
+            return;
+        }
+        this.pokemons.forEach((pokemon) => {
+            console.log(
+                `#${pokemon.id} - ${pokemon.nome} | Tipos: ${pokemon.tipos.join(", ")}`
+            );
+        });
+
+    }
+    remover(id:number): void {
+        const existe = this.pokemons.some((pokemon) => pokemon.id === id);
+        if (!existe) {
+            console.log("[AVISO] Nenhum Pokémon encontrado com esse ID.");
+            return;
+
+        }
+        this.pokemons = this.pokemons.filter((pokemon) => pokemon.id !== id);
+        console.log("[OK] Pokémon removido do catálogo.");
+    }
 }
